23284105e9d0ac7de2d5557386e1202a3a8a7666
XPL0Z <meneliksigrist2009@gmail.com>
2026-08-21 13:51:33 +0200

    [feature] parametrable error page

--- src/auth/RequireAuth.tsx
+++ src/auth/RequireAuth.tsx
@@ -1,18 +1,15 @@
+import ErrorPage from "@/pages/Error";
+import { LoadingPage } from "@/pages/Loading";
 import {type State, StateEnum} from "@epfl-si/react-appauth";
 import { Outlet } from "react-router";
 
 export const RequireAuth = ({ oidc }: { oidc: State }) => {
   if (oidc.state === StateEnum.InProgress) {
-    return <div className="flex justify-center items-center h-full p-10">Chargement...</div>;
+    return <LoadingPage/>;
   }
 
   if (oidc.state !== StateEnum.LoggedIn) {
-    return (
-      <div className="flex flex-col items-center justify-center py-20 text-center">
-        <h2 className="text-2xl font-bold mb-4">Accès restreint</h2>
-        <p className="text-gray-600 mb-6">Vous devez être connecté pour voir cette page.</p>
-      </div>
-    );
+    return <ErrorPage errorCode={401} message="errors.unauthenticated.title"/>
   }
 
   return <Outlet />;
