a158ebed120928e01dffe69f3bc7a588e8f1bfe3
XPL0Z <meneliksigrist2009@gmail.com>
2026-09-11 09:13:33 +0200

    [refactor] replace loading message with LoadingPage component in RequireAuth

--- src/auth/RequireAuth.tsx
+++ src/auth/RequireAuth.tsx
@@ -1,16 +1,16 @@
-import ErrorPage from "@/pages/Error";
-import { LoadingPage } from "@/pages/Loading";
-import {type State, StateEnum} from "@epfl-si/react-appauth";
-import { Outlet } from "react-router";
+import ErrorPage from "@/pages/Error"
+import { LoadingPage } from "@/pages/Loading"
+import { type State, StateEnum } from "@epfl-si/react-appauth"
+import { Outlet } from "react-router"
 
 export const RequireAuth = ({ oidc }: { oidc: State }) => {
   if (oidc.state === StateEnum.InProgress) {
-    return <LoadingPage/>;
+    return <LoadingPage />
   }
 
   if (oidc.state !== StateEnum.LoggedIn) {
-    return <ErrorPage errorCode={401} message="errors.unauthenticated.title"/>
+    return <ErrorPage errorCode={401} message="errors.unauthenticated.title" />
   }
 
-  return <Outlet />;
-};
+  return <Outlet />
+}
