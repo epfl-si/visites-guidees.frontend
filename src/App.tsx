75f80cb70c3568ee53a779228a90f899c4341213
XPL0Z <meneliksigrist2009@gmail.com>
2026-09-17 08:30:05 +0200

    [fix] Error flash when going on a page with required Role
    And fix a small typo

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,9 @@ export default function App() {
     username: "",
     email: "",
   })
+  const connectedUserLoading =
+    oidc.state === StateEnum.InProgress ||
+    (oidc.state === StateEnum.LoggedIn && !connectedUser.username)
 
   useEffect(() => {
     if (oidc.state === StateEnum.LoggedIn && oidc.accessToken) {
@@ -85,7 +88,13 @@ export default function App() {
             ))}
             <Route element={<RequireAuth oidc={oidc} />}>
               <Route
-                element={<RequireRole role="admin" user={connectedUser} />}
+                element={
+                  <RequireRole
+                    role="admin"
+                    user={connectedUser}
+                    loading={connectedUserLoading}
+                  />
+                }
               >
                 <Route element={<AdminLayout />}>
                   {/* All routes that here require admin permission */}
--- src/auth/RequireRole.tsx
+++ src/auth/RequireRole.tsx
@@ -1,17 +1,23 @@
 import { Outlet } from "react-router"
 import type { UserType } from "@/types/user"
 import ErrorPage from "@/pages/Error"
+import { LoadingPage } from "@/pages/Loading"
 
 export const RequireRole = ({
   user,
   role,
+  loading,
 }: {
   user: UserType
   role: string
+  loading: boolean
 }) => {
+  if (loading) {
+    return <LoadingPage />
+  }
 
   if (!user.roles.includes(role)) {
-    return <ErrorPage errorCode={403} message="errors.forbidden.title"/>
+    return <ErrorPage errorCode={403} message="errors.forbidden.title" />
   }
 
   return <Outlet />
