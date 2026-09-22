05f06442ec28e8c5867aefad4d5ce471be1dd435
antoine <antoine.fabre@epfl.ch>
2026-09-22 12:24:17 +0200

    [feature] Add dynamic status in workflow

--- src/components/reservations/snake-status.tsx
+++ src/components/reservations/snake-status.tsx
@@ -5,6 +5,7 @@ import { useTranslation } from "react-i18next";
 import { STATUS_ORDER } from "@/constants/status";
 import type { ReservationStep } from "@/types/reservation";
 import type { StepStatus } from "@/types/status";
+import { cn } from "@/lib/utils";
 
 export function SnakeStatus({ status }: { status: ReservationStatus }) {
   const { t } = useTranslation();
@@ -26,11 +27,13 @@ export function SnakeStatus({ status }: { status: ReservationStatus }) {
 
       return {
         reservationStatus: flowStatus,
-        label: t(`reservation.status.${flowStatus.toLowerCase()}`),
+        label: t(`reservation.steps.${flowStatus.toLowerCase()}`),
         status: stepStatus,
       };
-    })
-  }, [status, t])
+    });
+  }, [status, t]);
+
+  console.log("steps : ", steps);
 
   return (
     <div className="w-full max-w-sm mx-auto p-10 flex flex-col">
@@ -38,19 +41,34 @@ export function SnakeStatus({ status }: { status: ReservationStatus }) {
       <div className="flex w-full">
         <div className="flex flex-col w-12 shrink-0 h-40">
           <div className="h-1/2 w-full border-t-4 border-l-4 border-emerald-600 rounded-tl-full relative">
-            <div className="absolute -bottom-6 -left-5.5 flex h-12 w-12 items-center justify-center rounded-full bg-emerald-600">
-              <Check className="text-background h-7 w-7" />
+            <div
+              className={
+                cn("absolute -bottom-6 -left-6.5 flex h-12 w-12 items-center justify-center rounded-full",
+                  steps[0].status === "success" && "bg-emerald-600 text",
+                  steps[0].status === "in-progress" && "bg-yellow-600",
+                  steps[0].status === "pending" && "bg-accent text-muted-foreground"
+                )
+              }
+            >
+              {steps[0].status === "success" && (
+                <Check className="h-7 w-7 text-background" />
+              )}
+              {steps[0].status === "in-progress" && (
+                <div className="h-4 w-4 rounded-full bg-white animate-pulse" />
+              )}
+
+              <p className="absolute left-1/2 top-full mt-2 -translate-x-1/2 whitespace-nowrap text-center text-sm font-medium">
+                {steps[0]?.label}
+              </p>
             </div>
           </div>
           <div className="h-1/2 w-full border-b-4 border-l-4 border-accent rounded-bl-full" />
         </div>
 
-        <div className="flex-1 border-t-4 border-emerald-600 relative">
-          <div className="absolute -top-6 right-0 flex flex-col items-center translate-x-1/2">
-            <div className="flex h-12 w-12 shrink-0 items-center justify-center rounded-full bg-emerald-600">
-              <Check className="text-background h-7 w-7" />
-            </div>
-            <p className="mt-2 text-sm font-medium whitespace-nowrap text-center">
+        <div className="relative flex-1 border-t-4 border-emerald-600">
+          <div className="absolute -top-6 right-0 flex h-12 w-12 translate-x-1/2 items-center justify-center rounded-full bg-emerald-600">
+            <Check className="h-7 w-7 text-background" />
+            <p className="absolute left-1/2 top-full mt-2 -translate-x-1/2 whitespace-nowrap text-center text-sm font-medium">
               {t("reservation.steps.reserved")}
             </p>
           </div>
@@ -64,7 +82,25 @@ export function SnakeStatus({ status }: { status: ReservationStatus }) {
         <div className="flex-1 border-t-4 border-accent" />
         <div className="flex flex-col w-12 shrink-0 h-40">
           <div className="h-1/2 w-full border-t-4 border-r-4 border-accent rounded-tr-full relative">
-            <div className="absolute -bottom-6 -right-5.5 h-12 w-12 rounded-full bg-accent" />
+            <div
+              className={
+                cn("absolute -bottom-6 -right-6.5 flex h-12 w-12 items-center justify-center rounded-full",
+                  steps[1].status === "success" && "bg-emerald-600 text",
+                  steps[1].status === "in-progress" && "bg-yellow-600",
+                  steps[1].status === "pending" && "bg-accent text-muted-foreground"
+                )
+              }
+            >
+              {steps[1].status === "success" && (
+                <Check className="h-7 w-7 text-background" />
+              )}
+              {steps[1].status === "in-progress" && (
+                <div className="h-4 w-4 rounded-full bg-white animate-pulse" />
+              )}
+              <p className="absolute left-1/2 top-full mt-2 -translate-x-1/2 whitespace-nowrap text-center text-sm font-medium">
+                {steps[1]?.label}
+              </p>
+            </div>
           </div>
           <div className="h-1/2 w-full border-b-4 border-r-4 border-accent rounded-br-full" />
         </div>
@@ -73,7 +109,25 @@ export function SnakeStatus({ status }: { status: ReservationStatus }) {
       <div className="flex w-full -mt-1">
         <div className="flex flex-col w-12 shrink-0 h-40">
           <div className="h-1/2 w-full border-t-4 border-l-4 border-accent rounded-tl-full relative">
-            <div className="absolute -bottom-6 -left-5.5 h-12 w-12 rounded-full bg-accent" />
+            <div
+              className={
+                cn("absolute -bottom-6 -left-5.5 flex h-12 w-12 items-center justify-center rounded-full",
+                  steps[2].status === "success" && "bg-emerald-600 text",
+                  steps[2].status === "in-progress" && "bg-yellow-600",
+                  steps[2].status === "pending" && "bg-accent text-muted-foreground"
+                )
+              }
+            >
+              {steps[2].status === "success" && (
+                <Check className="h-7 w-7 text-background" />
+              )}
+              {steps[2].status === "in-progress" && (
+                <div className="h-4 w-4 rounded-full bg-white animate-pulse" />
+              )}
+              <p className="absolute left-1/2 top-full mt-2 -translate-x-1/2 whitespace-nowrap text-center text-sm font-medium">
+                {steps[2]?.label}
+              </p>
+            </div>
           </div>
           <div className="h-1/2 w-full border-b-4 border-l-4 border-accent rounded-bl-full" />
         </div>
@@ -83,9 +137,27 @@ export function SnakeStatus({ status }: { status: ReservationStatus }) {
 
       <div className="flex w-full -mt-1">
         <div className="w-12 shrink-0" />
-        <div className="flex-1 border-t-4 border-accent" />
-        <div className="-bottom-6 flex h-12 w-12 items-center justify-center rounded-full bg-emerald-600" />
-
+        <div className="relative flex-1 border-t-4 border-accent">
+          <div
+            className={
+              cn("absolute -top-6 right-0 flex h-12 w-12 translate-x-1/2 items-center justify-center rounded-full",
+                steps[3].status === "success" && "bg-emerald-600 text",
+                steps[3].status === "in-progress" && "bg-yellow-600",
+                steps[3].status === "pending" && "bg-accent text-muted-foreground"
+              )
+            }
+          >
+            {steps[3].status === "success" && (
+              <Check className="h-7 w-7 text-background" />
+            )}
+            {steps[3].status === "in-progress" && (
+              <div className="h-4 w-4 rounded-full bg-white animate-pulse" />
+            )}
+            <p className="absolute left-1/2 top-full mt-2 -translate-x-1/2 whitespace-nowrap text-center text-sm font-medium">
+              {steps[3]?.label}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
